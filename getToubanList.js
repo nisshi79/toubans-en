5f38a8b505e97a378bde5e696da6f1dbcf83c23f
Yui Nishimura <nisshi.yui79@gmail.com>
2018-11-18 17:18:52 +0900

    userAgent

--- getToubanList.js
+++ getToubanList.js
@@ -53,15 +53,33 @@ function initializeApp(data) {
 
     $('.add_touban').attr('href', 'mainForm.php?groupId='+lineId);
 
-    fetch('getToubanList.php?groupId=' + lineId)
+    fetch('getToubanList.php?groupId='+lineId)
         .then(response => {
             return response.json(); // ReadableStream -> String への変換
         })
         .then(json => {
             json.forEach(function (touban) {
                 $('.toubanList')
-                    .append(`<li><a href="mainForm.php?groupId=${lineId}&tableId=${touban.table.id}">`+touban.role[0].role+' の設定を変更</a></li>');
+                    .append(`<li><a class="black_font large_font">${touban.role[0].role}: </a><a class="btn" href="mainForm.php?groupId=${lineId}&tableId=${touban.table.id}">設定変更</a> <a id="${touban.table.id}" class="btn touban_del" href="javascript:void(0)">削除</a></li>`);
+                $('.touban_del').off('click');
+                $('.touban_del').on('click', function () {
+
+                    if(window.confirm('本当によろしいですか？')){
+                        fetch('delTouban.php?toubanId='+$(this).attr('id')+'&groupId='+lineId)
+                            .then(response => {
+                                return response.json(); // ReadableStream -> String への変換
+                            })
+                            .then(json => {
+                                $('.toubanList').empty();
+                                json.forEach(function (touban) {
+                                    $('.toubanList')
+                                        .append(`<li><a class="black_font large_font">${touban.role[0].role}: </a><a class="btn" href="mainForm.php?groupId=${lineId}&tableId=${touban.table.id}">設定変更</a> <a id="${touban.table.id}" class="btn touban_del" href="javascript:void(0)">削除</a></li>`);
+                                });
+                            });;
+                    }
+                });
             });
         });
 
+    $('.add_touban').attr('href', 'mainForm.php?groupId='+lineId);
 }
